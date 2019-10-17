49f6a1c654f071fd6ac7ee052b7c52362938aa24
julian <mueller@lotum.de>
2019-10-17 15:31:10 +0200

    codesplit

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
 	entry:{
 		app: './src/index.js',
 		print: './src/print.js',
+		another: './src/another-module.js',
 	},
 	plugins: [
+		new BundleAnalyzerPlugin(),
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			title: "Output Management",
@@ -16,5 +19,10 @@ module.exports = {
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+		},
 	},
 }
